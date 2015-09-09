1aa884f140e746c02680555b187b836abc84a581
moshuqi <270304102@qq.com>
2015-09-09 22:22:58 +0800

    Initial Commit

---
+++ PieceDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PieceDiary
+//
+//  Created by moshuqi on 15/9/9.
+//  Copyright (c) 2015年 msq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
